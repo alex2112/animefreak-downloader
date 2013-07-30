da55bdfe9a8fe81b9b02fb5a6aef0a62b1c1fa7b
alex2112 <alexsterg@gmail.com>
2013-07-30 12:49:40 +0300

    added mozilla user agent, search works with more than 1 word, now cat detect upload2.com and mp4upload.com mirrors, alternative and better url decoder, various usability improvements and code cleanup

--- animefreak-downloader.sh
+++ animefreak-downloader.sh
@@ -1,84 +1,86 @@
 #!/bin/bash
 
+### CONFIGURATION ###
 download_path=$HOME/Downloads # Defaults: $HOME/Downloads
 temp_path=/tmp/animefreak_downloader # Defaults: /tmp/animefreak_downloader
-searchterm=$@ # DO NOT TOUCH THIS!
+user_agent="Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:22.0) Gecko/20100101 Firefox/22.0"
+### END CONFIGURATION ###
 
-cd $temp_path 2>/dev/null || mkdir -p $temp_path && cd $temp_path
+search_term=$@ # DO NOT TOUCH THIS!
+
+cd $temp_path || mkdir -p $temp_path && cd $temp_path
+exec 2>log.txt
+
+function mirror_detect {
+episode_title=$(echo "$episodes" | sed -n "$ep"p | grep -o ' .*$' | sed 's/ //')
+filename=$(echo "$episodes" | sed -n "$ep"p | grep -o ' .*$' | sed -e 's/ //' -e 's/ /_/g' -e 's/$/.mp4/')
+page=$(echo "$episodes" | sed -n "$ep"p | sed 's/ .*$//' | wget -nv -U "$user_agent" -i - -O - | grep -e Fst -e upload2 -e mp4upload)
+mirrors=$(url_decoder "$page" | grep -o -e "http..*&st=......................" -e "http..*&e=.........." -e '"http..*upload2..*embed/.*"' -e "http.*mp4upload.*\.html" | sort -u)
+num_of_mirrors=$(echo "$mirrors" | wc  | awk {'print $1'})
+echo "INFO: Number of mirrors detected $num_of_mirrors which are: " >> log.txt # debug
+echo "$mirrors" >> log.txt # debug
+}
 
 function mirror {
-num_of_mirrors=$(echo "$mirrors" | wc  | awk {'print $1'})
-# echo "$mirrors" # debug
-if [ $num_of_mirrors -eq 1 ]
-then
-	mirror_type=$(echo $mirrors | grep -o "var tempfile")
-	if [ "$mirror_type" == "var tempfile" ]
+while :
+do
+	if [ "$num_of_mirrors" -eq 1 ]
 	then
-		# echo "video_url2" # debug
 		mirror_num=1
-		video_url2
-	else
-		# echo "video_url" # debug
-		mirror_num=1
-		video_url
+		mirror_type
+		break
 	fi
-elif [ $num_of_mirrors -eq 0 ]
+	clear
+	read -p "There are $num_of_mirrors mirrors for $episode_title. Select a number and press enter, (b) to go back, (q) to quit. >> " mirror_num 2>&1
+	if [ "$mirror_num" == q ]
+	then
+		echo "exit"
+		exit
+	elif [ "$mirror_num" == b ]
+	then
+		break
+	elif ! [[ "$mirror_num" =~ ^[0-9]+$ ]] ; 
+	then
+		echo "Not a number"
+		sleep 1
+		continue
+	elif [ "$mirror_num" == 0 -o "$mirror_num" -gt "$num_of_mirrors" ]
+	then
+		echo "Not a valid number"
+		sleep 1
+		continue
+	else
+		mirror_type
+		break
+	fi
+done
+}
+
+function mirror_type {
+type_of_mirror=$(echo "$mirrors" | sed -n "$mirror_num"p | grep -o -e "animefreak.tv" -e "upload2" -e "mp4upload")
+if [ "$type_of_mirror" == "animefreak.tv" ]
 then
-	read -p "Can't detect a working mirror :(. Press enter to continue..."
-	break
+	downlink=$(wget -nv -U "$user_agent" "$(echo "$mirrors" | sed -n "$mirror_num"p)" -O - | grep movie)
+	url=$(url_decoder "$downlink" | grep -o -e "=http..*st=......................" -e "=http..*e=.........." | sed 's/=//')
+elif [ "$type_of_mirror" == upload2 ]
+then
+	downlink=$(wget -nv -U "$user_agent" "$(echo "$mirrors" | sed -n "$mirror_num"p | sed 's/"//')" -O - | grep movie)
+	url=$(echo "$downlink" | grep -o "video=http..*" | sed 's/rating=/\n/' | sed -n 1p | sed -e 's/video=//' -e 's/&$//')
+elif [ "$type_of_mirror" == mp4upload ]
+then
+	downlink=$(wget -nv -U "$user_agent" "$(echo "$mirrors" | sed -n "$mirror_num"p)" -O - | grep "'file'")
+	url=$(echo "$downlink" | grep -o "http..*'" | sed "s/'//")
 else
-	while :
-	do
-		clear
-		echo "There are $num_of_mirrors mirrors for $animetitle"
-		echo "Select a number and press enter"
-		read mirror_num
-		if [ "$mirror_num" == q ]
-		then
-			echo "exit"
-			exit
-		elif ! [[ "$mirror_num" =~ ^[0-9]+$ ]] ; 
-		then
-			echo "Not a number"
-			sleep 1
-			continue
-		elif [ "$mirror_num" == 0 -o "$mirror_num" -gt "$num_of_mirrors" ]
-		then
-			echo "Not a valid number"
-			sleep 1
-			continue
-		else
-			mirror_type=$(echo "$mirrors" | sed -n "$mirror_num"p | grep -o "var tempfile")
-			if [ "$mirror_type" == "var tempfile" ]
-			then
-				# echo "video_url2" # debug
-				video_url2
-				break
-			else
-				# echo "video_url" # debug
-				video_url
-				break
-			fi
-		fi
-	done
-fi	
-}
-
-function video_url {
-url=$(echo "$mirrors" | sed -n "$mirror_num"p | url_decoder | grep -o "http..*e=..........")
-# echo $url # debug
-}
-
-function video_url2 {
-url=$(wget -q "$(echo "$mirrors" | sed -n "$mirror_num"p | url_decoder | grep -o "http..*st=......................")" -O - | grep -m 1 -oi "file=..*..&captions" | url_decoder | sed -e 's/file=//' -e 's/\&captions//')
-# echo $url # debug
+	type_of_mirror=direct
+	url=$(echo "$mirrors" | sed -n "$mirror_num"p)
+fi
+echo "INFO: $type_of_mirror path chosen with url $url" >> log.txt # debug
 }
 
 function downloader {
 while :
 do
-	echo "View (v) or save (s) video? (b) to go back (q) to quit."
-	read choice
+	read -p "View (v) or save (s) video? (b) to go back (q) to quit. >> " choice 2>&1
 	if [ -z "$choice" ]
 	then
 		sleep 1
@@ -93,12 +95,11 @@ do
 	elif [ "$choice" == s ]
 	then
 		echo "Now saving $filename to $download_path"
-		wget "$url" -O $download_path/$filename
+		wget -U "$user_agent" "$url" -O $download_path/$filename 2>&1 | tee -a log.txt
 		break
 	elif [ "$choice" == v ]
 	then
-		mplayer -msglevel all=-1 "$url"
-		# mplayer "$url" # debug
+		mplayer -msglevel all=1 -user-agent "$user_agent" "$url"
 		break
 	else
 		echo "Not a valid option"
@@ -112,9 +113,8 @@ function diff_mirror {
 while :
 do
 	clear
-	echo "Try a different mirror? (y) for yes (n) to go back (q) to quit."
-	read diffmir
-	if [ "$diffmir" == y -o n ]
+	read -p "Try a different mirror? (y) for yes (n) to go back (q) to quit. >> " diffmir 2>&1
+	if [ "$diffmir" == y -o "$diffmir" == n ]
 	then
 		break
 	elif [ "$diffmir" == q ]
@@ -130,12 +130,8 @@ done
 }
 
 function url_decoder {
-sed -e 's/%21/!/g'  -e 's/%23/#/g' -e 's/%24/$/g' -e 's/%26/\&/g' -e "s/%27/'/g" -e 's/%28/(/g' -e 's/%29/)/g' -e 's/%2A/*/g' -e 's/%2B/+/g' -e 's/%2C/,/g' -e 's/%2F/\//g' -e 's/%3A/:/g' -e 's/%3B/;/g' -e 's/%3D/=/g' -e 's/%3F/?/g' -e 's/%40/@/g' -e 's/%5B/[/g' -e 's/%5D/]/g' -e 's/+/ /g'
+local d=${1//+/ }; printf '%b' "${d//%/\x}";
 }
-
-# function urldecoder {
-# local d=${1//+/ }; printf '%b' "${d//%/\x}";
-# }
 
 function help_page {
 echo "Usage: animefreak.sh [OPTION]... [SEARCH_TERM]...
@@ -155,68 +151,65 @@ Options:
 	-h 	     print this help tip"
 }
 
-if [ $1 == "-h" ] 2>/dev/null
+if [ $1 == "-h" ]
 then
 	help_page
 	exit
-elif [ -n "$searchterm" ] 
+elif [ -n "$search_term" ] 
 then
-	if [ -f book.htm ]
+	if ! [ -s book.htm ]
 	then 
-		clear
-	else
-		wget -q "http://www.animefreak.tv/book" -O book.htm
-		clear
+		echo "Getting anime catalog."
+		wget -U "$user_agent" "http://www.animefreak.tv/book" -O book.htm
 	fi
 	while :
 	do
 		while :
 		do
 			clear
-			book_search=$(grep -o '<a href="/watch..*</a>' book.htm |sed -e 's/<a href="/http:\/\/www.animefreak.tv/' -e 's/">/ /' -e 's/<\/a>//' | grep -i -e " .*$searchterm" 2>/dev/null)
-			num_of_results=$(echo "$book_search" | wc | awk '{print $1}')
-			if [ -z "$book_search" ]
+			list=$(grep -o '<a href="/watch..*</a>' book.htm |sed -e 's/<a href="/http:\/\/www.animefreak.tv/' -e 's/">/ /' -e 's/<\/a>//' | \
+			grep -i -e " .*$search_term")
+			num_of_results=$(echo "$list" | wc | awk '{print $1}')
+			if [ -z "$list" ]
 			then
-				echo "No title found containing the word(s) $searchterm."
-				sleep 1
-				echo "Search for what?"
-				read searchterm
+				read -p "No title found containing the word(s) $search_term. Search for:" search_term 2>&1
 				continue
 			elif [ "$num_of_results" -eq 1 ]
 			then
 				series=1
-				series_episodes=$(echo "$book_search" | sed -n "$series"p | sed 's/ .*$//' | wget -q -i - -O - | grep -i leaf | grep -o '<a href..*</a>' | sed -e 's/<a href="/http:\/\/www.animefreak.tv/' -e 's/">/ /' -e 's/<\/a>//' |awk ' !x[$0]++')
+				episodes=$(echo "$list" | sed -n "$series"p | sed 's/ .*$//' | wget -U "$user_agent" -i - -O - | grep -i leaf | \
+				grep -o '<a href..*</a>' | sed -e 's/<a href="/http:\/\/www.animefreak.tv/' -e 's/">/ /' -e 's/<\/a>//' | awk ' !x[$0]++')
 				break
 			else
-				echo "$book_search" | grep -o " .*$" | awk '{print NR, $0}' | column
-				echo "Type a number to select a series or (s) to search again then press enter. Press (q) to quit."
-				echo "Found $num_of_results results containing the word(s): $searchterm"
-				read series
-				if [ -z $series ] 2>/dev/null
+				echo "$list" | grep -o " .*$" | awk '{print NR, $0}' | column
+				read -p "Found $num_of_results results containing the word(s): $search_term. Type a number to select a series or (s) to search again then press enter. Press (q) to quit. >> " series 2>&1
+				if [ -z $series ]
 				then
 					sleep 1
 					continue
-				elif [ "$series" == q ] 2>/dev/null
+				elif [ "$series" == q ]
 				then
 					echo "exit" 
 					exit
-				elif [ $series == s ] 2>/dev/null
+				elif [ $series == s ]
 				then
-					echo "Search for what?"
-					read searchterm
+					read -p "Search for what? >> " search_term 2>&1
 					continue
-				elif ! [[ "$series" =~ ^[0-9]+$ ]] 2>/dev/null
+				elif ! [[ "$series" =~ ^[0-9]+$ ]]
 				then
 					echo "Not a number"
 					sleep 1
 					continue
-				elif [ "$series" -gt "$num_of_results"  -o  "$series" -eq 0 ] 2>/dev/null
+				elif [ "$series" -gt "$num_of_results"  -o  "$series" -eq 0 ]
 				then
 					echo "Not a valid number"
 					sleep 1
 					continue
 				else
-					series_episodes=$(echo "$book_search" | sed -n "$series"p | sed 's/ .*$//' | wget -q -i - -O - | grep -i leaf | grep -o '<a href..*</a>' | sed -e 's/<a href="/http:\/\/www.animefreak.tv/' -e 's/">/ /' -e 's/<\/a>//' |awk ' !x[$0]++')
+					echo "Getting episode list."
+					episodes=$(echo "$list" | sed -n "$series"p | sed 's/ .*$//' | wget -U "$user_agent" -i - -O - | grep -i leaf | \
+					grep -o '<a href..*</a>' | sed -e 's/<a href="/http:\/\/www.animefreak.tv/' -e 's/">/ /' -e 's/<\/a>//' | \
+					awk ' !x[$0]++')
 					break
 				fi
 			fi
@@ -224,92 +217,99 @@ then
 		while :
 		do
 			clear
-			echo "$series_episodes" | grep -o -i " .*$" | awk '{print NR, $0}' |column  -t |more
+			echo "$episodes" | grep -o -i " .*$" | awk '{print NR, $0}' | column  -t | more
 			if [ "$num_of_results" -eq 1 ]
 			then
-				echo "Select an episode, (q) to quit."
+				read -p "Select an episode, (s) to make a new search, (q) to quit. >> " ep 2>&1
 			else
-				echo "Select an episode, (q) to quit, (b) to go back."
+				read -p "Select an episode, (b) to go back to search, (q) to quit. >> " ep 2>&1
 			fi
-			read ep
-			if [ "$ep" == q ] 2>/dev/null
+			if [ "$ep" == q ]
 			then
 				echo "exit"
 				exit
-			elif [ "$ep" == b -a "$num_of_results" != 1 ] 2>/dev/null
+			elif [ "$ep" == s ]
+			then
+				read -p "Search for what? >> " search_term 2>&1
+				break
+			elif [ "$ep" == b -a "$num_of_results" != 1 ]
 			then
 				break
-			elif ! [[ "$ep" =~ ^[0-9]+$ ]] 2>/dev/null
+			elif ! [[ "$ep" =~ ^[0-9]+$ ]]
 			then
 				echo "Not a number"
 				sleep 1
 				continue
 			else
-				animetitle=$(echo "$series_episodes" | sed -n "$ep"p | grep -o ' .*$' | sed 's/ //')
-				filename=$(echo "$series_episodes" | sed -n "$ep"p | grep -o ' .*$' | sed -e 's/ //' -e 's/ /_/g' -e 's/$/.mp4/')
-				mirrors=$(echo "$series_episodes" | sed -n "$ep"p | sed 's/ .*$//' | wget -q -i - -O - | grep -e "%3Fst%3D" -e "var tempfile")
-				break
+				mirror_detect
+				if [ -z "$mirrors" ]
+				then
+					read -p "Can't detect a working mirror :(. Press enter to continue." 2>&1
+					continue
+				fi
 			fi
-		done
-		while :
-		do
-			if [ "$ep" == b ]
-			then
-				break
-			fi
-			mirror
-			downloader
-			if [ "$num_of_mirrors" -eq 1 ]
-			then
-				break
-			else
-				diff_mirror
-			fi
-			if [ "$diffmir" == y ]
-			then
-				continue
-			else
-				break
-			fi
+			while :
+			do
+				mirror
+				if [ "$mirror_num" == b ]
+				then
+					break
+				fi
+				downloader
+				if [ "$num_of_mirrors" -eq 1 ]
+				then
+					break
+				else
+					diff_mirror
+				fi
+				if [ "$diffmir" == y ]
+				then
+					continue
+				else
+					break
+				fi
+			done
 		done
 	done
 else
-	latest=$(wget -q 'http://www.animefreak.tv/tracker' -O - | grep -o '"/watch.*</a>' | grep -i -e episode -e movie | sed -e 's/"/http:\/\/www.animefreak.tv/' -e 's/">/ /' -e 's/<\/a>//')
+	echo "Getting latest episodes."
+	episodes=$(wget -U "$user_agent" 'http://www.animefreak.tv/tracker' -O - | grep -o '"/watch.*</a>' | grep -i -e episode -e movie | \
+	sed -e 's/"/http:\/\/www.animefreak.tv/' -e 's/">/ /' -e 's/<\/a>//')
 	while :
 	do
-		while :
-		do
-			clear
-			num_of_results=$(echo "$latest" | wc | awk '{print $1}')
-			echo "$latest" | grep -o " .*$" | awk '{print NR, $0}' | more
-			echo "*************************************************************************"
-			echo "* Type a number to select an episode and press enter. Press (q) to quit *"
-			echo "*************************************************************************"
-			read laep
-			if [ "$laep" == q ]
+		clear
+		num_of_results=$(echo "$episodes" | wc | awk '{print $1}')
+		echo "$episodes" | grep -o " .*$" | awk '{print NR, $0}' | more
+		read -p "Type a number to select an episode and press enter. Press (q) to quit. >> " ep 2>&1
+		if [ "$ep" == q ]
+		then
+			echo "exit"
+			exit
+		elif ! [[ "$ep" =~ ^[0-9]+$ ]]
+		then
+			echo "Not a number"
+			sleep 1
+			continue
+		elif [ "$ep" -gt "$num_of_results" -o "$ep" -eq 0 ]
+		then
+			echo "Not a valid number"
+			sleep 1
+			continue
+		else
+			mirror_detect
+			if [ -z "$mirrors" ]
 			then
-				echo "exit"
-				exit
-			elif ! [[ "$laep" =~ ^[0-9]+$ ]] ;
-			then
-				echo "Not a number"
-				sleep 1
+				read -p "Can't detect a working mirror :(. Press enter to go back..." 2>&1
 				continue
-			elif [ "$laep" -gt "$num_of_results" -o "$laep" -eq 0 ]
-			then
-				echo "Not a valid number"
-				sleep 1
-				continue
-			else
-				animetitle=$(echo "$latest" | sed -n "$laep"p | grep -o ' .*$' | sed 's/ //')
-				filename=$(echo "$latest" | sed -n "$laep"p | grep -o ' .*$' | sed -e 's/ //' -e 's/ /_/g' -e 's/$/.mp4/')
-				mirrors=$(echo "$latest" | sed -n "$laep"p | sed 's/ .*$//' | wget -q -i - -O - | grep -e "%3Fst%3D" -e "var tempfile")
-				break
 			fi
-		done
+		fi
 		while :
 		do
 			mirror
+			if [ "$mirror_num" == b ]
+			then
+				break
+			fi
 			downloader
 			if [ "$num_of_mirrors" -eq 1 ]
 			then
